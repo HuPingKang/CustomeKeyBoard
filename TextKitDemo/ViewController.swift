30547ddd3e798a68cd17e9dc7ad40d2c532743df
Hpk <13456864167@163.com>
2018-03-15 17:08:58 +0800

    A custom keyboard

--- TextKitDemo/ViewController.swift
+++ TextKitDemo/ViewController.swift
@@ -10,11 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+
+    @IBOutlet var enterView: UIView!
+    @IBOutlet var toolBar: UIToolbar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.textView.inputAccessoryView = self.toolBar
+        self.textView.inputView = self.enterView
+        
+    }
+    @IBAction func clickDone(_ sender: Any) {
+        self.textView.resignFirstResponder()
+    }
+    
+    @IBAction func head(_ sender: Any) {
+        
+        self.textView.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        
+    }
+    
+    @IBAction func H(_ sender: Any) {
+        self.textView.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+    
+    @IBAction func SH(_ sender: Any) {
+        self.textView.font = UIFont.preferredFont(forTextStyle: .title2)
+    }
+    
+    @IBAction func B(_ sender: Any) {
+        self.textView.font = UIFont.preferredFont(forTextStyle: .title3)
+    }
+    
+    @IBAction func F(_ sender: Any) {
+        self.textView.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+    
+    @IBAction func C1(_ sender: Any) {
+        self.textView.font = UIFont.preferredFont(forTextStyle: .subheadline)
+    }
+    
+    @IBAction func C2(_ sender: Any) {
+        self.textView.font = UIFont.preferredFont(forTextStyle: .body)
+    }
+    
+    @IBAction func EP(_ sender: Any) {
+        self.textView.font = UIFont.preferredFont(forTextStyle: .callout)
+        
+    }
+    
+    @IBAction func foot(_ sender: Any) {
+        self.textView.font = UIFont.preferredFont(forTextStyle: .footnote)
+        
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
